b774323af263b90841e51b6826602e289cb37bb0
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-11 13:21:03 -0300

    Alterações no main - ex11

--- lib/Exemplo-11/ex11/lib/main.dart
+++ lib/Exemplo-11/ex11/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ex11/tecla.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,59 +17,46 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, }) : super(key: key);
 
-  final String title;
+  
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
    
     return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
+      
       body: Center(
        
         child: Column(
          
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: const[
+            Tecla(cor: Colors.red),
+            Tecla(cor: Colors.orange), 
+            Tecla(cor: Colors.yellow), 
+            Tecla(cor: Colors.green), 
+            Tecla(cor: Colors.teal), 
+            Tecla(cor: Colors.blue), 
+            Tecla(cor: Colors.purple)
+            // TODO: adicionar as teclas aqui
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
