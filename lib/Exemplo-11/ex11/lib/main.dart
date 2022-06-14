f868cd9a734c63a7a2a1ae88520cb04e51b68155
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-14 17:06:38 -0300

    Adição de teclas no main - ex11

--- lib/Exemplo-11/ex11/lib/main.dart
+++ lib/Exemplo-11/ex11/lib/main.dart
@@ -33,26 +33,28 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
    
-    return Scaffold(
-      
-      body: Center(
-       
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        
+        body: Center(
          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const[
-            Tecla(cor: Colors.red),
-            Tecla(cor: Colors.orange), 
-            Tecla(cor: Colors.yellow), 
-            Tecla(cor: Colors.green), 
-            Tecla(cor: Colors.teal), 
-            Tecla(cor: Colors.blue), 
-            Tecla(cor: Colors.purple)
-            // TODO: adicionar as teclas aqui
-          ],
+          child: Column(
+           
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Tecla(cor: Colors.red, numero: 1,),
+              Tecla(cor: Colors.orange, numero: 2,), 
+              Tecla(cor: Colors.yellow, numero: 3,), 
+              Tecla(cor: Colors.green, numero: 4,), 
+              Tecla(cor: Colors.teal, numero: 5,), 
+              Tecla(cor: Colors.cyan, numero: 6,), 
+              Tecla(cor: Colors.purple, numero: 7,)
+             
+            ],
+          ),
         ),
+        // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
