e1b48724d14a9b606d4318d105a9204a8a399297
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-17 19:26:34 -0300

    Modificações: cartão idade/peso - ex12

--- lib/Exemplo-12/ex12/lib/cartao_idade_peso.dart
+++ lib/Exemplo-12/ex12/lib/cartao_idade_peso.dart
@@ -8,18 +8,39 @@ class CartaoIdadePeso extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column( 
-      // TODO: Terminar o widget Cartão Idade Peso
-      children: [ 
-        Text(''), 
-        Text(''), 
-        Row( 
-          children: [ 
-            Container(), 
-            Container(),
-          ],
-        ),
-      ],
+    return Center(
+      child: Column( 
+        mainAxisAlignment: MainAxisAlignment.start,
+        
+        children: [ 
+          Text('$tipo', style: const TextStyle(fontSize: 15.0),), 
+          const Text('74', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)), 
+          Row( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [ 
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Container( 
+                  height: 40,
+                  width: 40,
+                  child: Icon(Icons.remove),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0), color: Colors.white),
+                ),
+              ),
+      
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Container( 
+                  height: 40,
+                  width: 40,
+                  child: Icon(Icons.add),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0), color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
