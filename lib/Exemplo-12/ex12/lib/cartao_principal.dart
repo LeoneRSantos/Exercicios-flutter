2e14920aa719f025ea1c61384b73a855b1c182f7
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-16 22:37:25 -0300

    Cartão principal - ex12

---
+++ lib/Exemplo-12/ex12/lib/cartao_principal.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CartaoPrincipal extends StatelessWidget {
+  const CartaoPrincipal({Key? key, required this.conteudo}) : super(key: key);
+
+  final Widget conteudo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded( 
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container( 
+          height: 100,
+          child: conteudo,
+          decoration: BoxDecoration( 
+            color: Color(0xFF4C5065),
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ));
+  }
+}
