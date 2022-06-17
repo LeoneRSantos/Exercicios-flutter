3627cd74f909b954297eb680c145f00b744e738f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-16 22:39:12 -0300

    Cartão idade/peso - ex12

---
+++ lib/Exemplo-12/ex12/lib/cartao_idade_peso.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CartaoIdadePeso extends StatelessWidget {
+  const CartaoIdadePeso({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column( 
+      // TODO: Terminar o widget Cartão Idade Peso
+      children: [ 
+        Text(''), 
+        Text(''), 
+        Row( 
+          children: [ 
+            Container(), 
+            Container(),
+          ],
+        ),
+      ],
+    );
+  }
+}
