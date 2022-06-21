3e465dff50df69ed0bfec57f839aa77bf4d8f5db
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-21 19:06:35 -0300

    Criação do Botão Re-calcular - ex12

---
+++ lib/Exemplo-12/ex12/lib/widgets/botao_recalcular.dart
@@ -0,0 +1,19 @@
+import 'package:ex12/pages/tela_principal.dart';
+import 'package:flutter/material.dart';
+
+class BotaoReCalcular extends StatelessWidget {
+  const BotaoReCalcular({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 60, 
+      decoration: const BoxDecoration(color: Color(0xFFFD0225),),
+      child: TextButton(onPressed: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => TelaPrincipal()),
+        );
+      },  
+        child: Text('re-calcular'.toUpperCase(), style: TextStyle(color: Color(0xFFfdfcfe)),)),
+    );
+  }
+}
