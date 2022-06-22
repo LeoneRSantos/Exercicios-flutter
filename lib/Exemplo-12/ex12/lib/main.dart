082bd2b0f8074a5d487f099ed9988f3d40a7bff2
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-22 09:06:16 -0300

    Retirada do debug banner - ex12

--- lib/Exemplo-12/ex12/lib/main.dart
+++ lib/Exemplo-12/ex12/lib/main.dart
@@ -7,6 +7,7 @@ class CalculadoraIMC extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF14163B),
         scaffoldBackgroundColor: Color(0xFF14163B),
