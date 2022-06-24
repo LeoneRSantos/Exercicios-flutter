712a14014574e27041d9fd653976f9dbf6f12b4f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-24 18:11:25 -0300

    Alterações no Cartão Peso - ex12

--- lib/Exemplo-12/ex12/lib/widgets/cartao_peso.dart
+++ lib/Exemplo-12/ex12/lib/widgets/cartao_peso.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class CartaoPeso extends StatefulWidget {
-  CartaoPeso({Key? key}) : super(key: key);
+  CartaoPeso({Key? key, required this.peso, required this.incrementar, required this.decrementar}) : super(key: key);
+
+  final int peso;
+  final void Function() incrementar;
+  final void Function() decrementar;
 
   @override
   State<CartaoPeso> createState() => _CartaoPesoState();
@@ -10,13 +14,13 @@ class CartaoPeso extends StatefulWidget {
 
 class _CartaoPesoState extends State<CartaoPeso> {
   int peso = 60;
-
+ 
   void incrementarPeso(){
     setState(() {
       peso++;
     });
   }
-
+ 
   void decrementarPeso(){
     setState(() {
       peso--;
@@ -31,7 +35,7 @@ class _CartaoPesoState extends State<CartaoPeso> {
         
         children: [ 
           Text('PESO', style: const TextStyle(fontSize: 15.0, color: Color(0xFFfdfcfe)),), 
-          Text('$peso', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Color(0xFFfdfcfe))), 
+          Text('${widget.peso}', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Color(0xFFfdfcfe))), 
           Row( 
             mainAxisAlignment: MainAxisAlignment.center,
             children: [ 
@@ -40,7 +44,7 @@ class _CartaoPesoState extends State<CartaoPeso> {
                 child: Container( 
                   height: 40,
                   width: 40,
-                  child: TextButton(child: Icon(Icons.remove), onPressed: decrementarPeso,),
+                  child: TextButton(child: Icon(Icons.remove), onPressed: (){widget.decrementar();},),
                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0), color: Color(0xFFfdfcfe)),
                 ),
               ),
@@ -50,7 +54,7 @@ class _CartaoPesoState extends State<CartaoPeso> {
                 child: Container( 
                   height: 40,
                   width: 40,
-                  child: TextButton(child: Icon(Icons.add), onPressed:incrementarPeso),
+                  child: TextButton(child: Icon(Icons.add), onPressed:(){widget.incrementar();},),
                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0), color: Color(0xFFfdfcfe)),
                 ),
               ),
