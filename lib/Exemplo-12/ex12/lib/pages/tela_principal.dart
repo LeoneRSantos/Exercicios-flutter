30c9a00a12f8dc3251acf7dee14842af9a14179c
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-24 18:14:10 -0300

    Alterações na Tela Principal - ex12

--- lib/Exemplo-12/ex12/lib/pages/tela_principal.dart
+++ lib/Exemplo-12/ex12/lib/pages/tela_principal.dart
@@ -5,6 +5,7 @@ import 'package:ex12/widgets/cartao_peso.dart';
 import 'package:ex12/widgets/cartao_principal.dart';
 import 'package:flutter/material.dart';
 import '../widgets/cartao_altura.dart';
+import 'dart:math';
 
 class TelaPrincipal extends StatefulWidget {
   @override
@@ -12,6 +13,31 @@ class TelaPrincipal extends StatefulWidget {
 }
 
 class _TelaPrincipalState extends State<TelaPrincipal> {
+
+  double imcAtual = 21.0;
+
+  int pesoAtual = 70, idade = 20, alturaAtual = 100;
+
+  String _auxResultado = '';
+
+  void incrementarPeso(){
+    setState(() {
+      pesoAtual++;
+    });
+  }
+
+  void decrementarPeso(){
+    setState(() {
+      pesoAtual--;
+    });
+  }
+
+  void calcularAltura(double d){
+    setState(() {
+      alturaAtual = d.toInt();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,17 +55,17 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
             ],
           ),
 
-          const CartaoPrincipal(conteudo: CartaoAltura()),
+          CartaoPrincipal(conteudo: CartaoAltura(altura: alturaAtual, alteraValor: (d) { calcularAltura(d); },)),
 
           Row( 
             children: [ 
-              CartaoPrincipal(conteudo: CartaoPeso(),),
+              CartaoPrincipal(conteudo: CartaoPeso(peso: pesoAtual, decrementar: () { decrementarPeso();  }, incrementar: () { incrementarPeso(); },),),
               CartaoPrincipal(conteudo: CartaoIdade()),
               
             ],
           ), 
 
-          const BotaoCalcular(),
+          BotaoCalcular(peso: pesoAtual, altura: alturaAtual),
         ],
       ),
      
