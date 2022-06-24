d33f48cfc036e9a983897def30fdaf68284ca170
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-24 18:13:15 -0300

    Botão Calcular - ex12

--- lib/Exemplo-12/ex12/lib/widgets/botao_calcular.dart
+++ lib/Exemplo-12/ex12/lib/widgets/botao_calcular.dart
@@ -1,16 +1,30 @@
+import 'dart:math';
+
 import 'package:ex12/pages/tela_resultado.dart';
 import 'package:flutter/material.dart';
 
 class BotaoCalcular extends StatelessWidget {
-  const BotaoCalcular({Key? key}) : super(key: key);
+  BotaoCalcular({Key? key, required this.peso, required this.altura}) : super(key: key);
+
+  late double resultadoImc;
+  late double imc;
+  final int peso;
+  final int altura;
+
+  double calcularImc(){
+    imc = (peso/pow(altura/100, 2));
+    debugPrint('Peso: $peso, altura: $altura, calculado: $imc');
+    return imc;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(height: 60, 
       decoration: const BoxDecoration(color: Color(0xFFFD0225),),
       child: TextButton(onPressed: (){
+        resultadoImc = calcularImc();
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => TelaResultado()),
+          MaterialPageRoute(builder: (context) => TelaResultado(imc: resultadoImc,)),
         );
       },  
         child: Text('calcular'.toUpperCase(), style: TextStyle(color: Color(0xFFfdfcfe)),)),
