bf6dea662e33a1f06f28b5359c32419e36989c4c
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-28 16:20:47 -0300

    Alterações no main - ex13

--- lib/Exemplo-13/ex13/lib/main.dart
+++ lib/Exemplo-13/ex13/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:ex13/fato_de_gato.dart';
 import 'package:flutter/material.dart';
-
 import 'package:dio/dio.dart';
 
 void main() {
@@ -39,17 +38,8 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
       
       body: FutureBuilder( 
@@ -66,20 +56,6 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
 
               return Center(
                 child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     const Text(
